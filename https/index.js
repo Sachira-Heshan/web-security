1a78c0904b33897a1e5ac0200d408e67ff8ab9eb
Sachira-Heshan <sachiraheshan99@gmail.com>
2023-11-22 21:01:09 +0530

    Added a new endpoint

--- https/index.js
+++ https/index.js
@@ -1,26 +1,40 @@
-import express from 'express'
-import fs from 'fs'
-import path from 'path'
-import https from 'https'
-import dotenv from 'dotenv'
-dotenv.config()
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const https = require("https");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 5000
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send("/ endpoint in https server")
-})
+const port = process.env.PORT || 5001;
 
-app.get('/api', (req, res) => {
-    res.send("/api endpoint in https server")
-})
+app.get("/", (req, res) => {
+   res.send("/ endpoint in https server");
+});
 
-https.createServer({
-    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
-}, app).listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-})
+app.get("/api", (req, res) => {
+   res.send("/api endpoint in https server");
+});
 
+app.post("/api/login", (req, res) => {
+   console.log(req.body);
+   const username = req.body.username;
+   const password = req.body.password;
+   res.send(`Username: ${username} and Password: ${password}`);
+});
+
+https
+   .createServer(
+      {
+         key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
+         cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
+      },
+      app
+   )
+   .listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+   });
